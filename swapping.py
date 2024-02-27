6ea6660565f258c344d7f1ffc15f7fda997f333a
AMAR-AP <114557114+AMAR-AP@users.noreply.github.com>
2024-02-27 08:56:07 +0530

    Create swapping.py

---
+++ swapping.py
@@ -0,0 +1,9 @@
+a=int(input("Enter value of A:"))
+b=int(input("Enter value of :"))
+print("Before swapping value:")
+print("A=",a,",B",b)
+a=a+b
+b=a-b
+a=a-b
+print("After swapping value:")
+print("A=",a,",B",b)
