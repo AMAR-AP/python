7ab15aee6b6ed923835092b52c3e4b5a75155663
AMAR-AP <114557114+AMAR-AP@users.noreply.github.com>
2024-02-27 08:49:23 +0530

    Create greatest_3.py

---
+++ greatest_3.py
@@ -0,0 +1,13 @@
+a=int(input("Enter value of A:"))
+b=int(input("Enter value of B:"))
+c=int(input("Enter value of C:"))
+if a>b:
+  if a>c:
+    print(a,"is greatest")
+  else:
+    print(c,"is greatest")
+else:
+  if b>c:
+    print(b,"is greatest")
+  else:
+     print(c,"is greatest")
